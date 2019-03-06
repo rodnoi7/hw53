8d6193ca999b1d30339332b416337236f719ac69
root <root@localhost.localdomain>
2019-03-07 03:24:20 +0900

    Finish!

--- src/App.js
+++ src/App.js
@@ -2,39 +2,48 @@ import React, { Component } from 'react';
 import {Num} from './components/index';
 import './App.css';
 
-class App extends Component {
-    constructor() {
-        let numbers = [];
-        for (let i=0; i<5; i++) {
-            let random_number = Math.floor(Math.random()*(36-5+1)+5);
-            if (numbers.indexOf(random_number) === -1) {
-                numbers.push(random_number);
-            }
-            else {
-                i-=1;
-                continue;
-            }
+function random_num(){
+    let numbers = [];   
+    for (let i=0; i<5; i++) {
+        let random_number = Math.floor(Math.random()*(36-5+1)+5);
+        if (numbers.indexOf(random_number) === -1) {
+            numbers.push(random_number);
         }
-
-        numbers.sort(function(a,b){ 
-            return a-b;
-        });
-        super();
-
-        this.state = {random_numbers: numbers};
-
+        else {
+            i-=1;
+            continue;
+        }
     }
 
+    numbers.sort(function(a,b){ 
+        return a-b;
+    });
+    console.log(numbers);
+    return (numbers);
+};
+
+class App extends Component {
+
+    state = {
+        numbers: random_num()
+    };
+
+    change_num = () => {
+        this.setState({
+            numbers: random_num()
+        })
+    }
     
     render() {
         return (
         <div className='App'>
             <Num 
-                num_1={this.state.random_numbers[0]} 
-                num_2={this.state.random_numbers[1]} 
-                num_3={this.state.random_numbers[2]} 
-                num_4={this.state.random_numbers[3]} 
-                num_5={this.state.random_numbers[4]} />
+                num_1={this.state.numbers[0]} 
+                num_2={this.state.numbers[1]} 
+                num_3={this.state.numbers[2]} 
+                num_4={this.state.numbers[3]} 
+                num_5={this.state.numbers[4]} />
+                <div><button onClick={this.change_num}>Change numbers</button></div>
         </div>
         );
    }
